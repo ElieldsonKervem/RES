b892c85abb477793ce72285184969858e274f87e
kervem <kervem.elieldson@academico.ifrn.edu.br>
2024-06-06 10:16:34 -0300

    createted index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+//constrole de versão, antes era enviado para um servidor loca hoje em dia tenho github
+
+//vemos como é facil criar um repositorio no github
+
+//commit é uma linha do tempo do projeto
